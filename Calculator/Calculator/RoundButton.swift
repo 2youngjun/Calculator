aa9ea3eda55643fe7309ec2f2cbb047aee058c40
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-07-15 12:37:00 +0900

    Calculator UI

---
+++ Calculator/Calculator/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by 이영준 on 2022/07/15.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+    
+
+}
